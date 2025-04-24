9fbc9c403c7d9c295278b930da62023cfbd943d8
faustinmukhwaya <faustin.mukhwaya@student.moringaschool.com>
2025-04-24 14:45:43 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,44 @@
-import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+// App.js
+import React from 'react';
 
+// Home component (already defined)
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>Welcome to my portfolio!</h1>
     </div>
   );
 }
 
-{/* write an <About> component here */}
+// Navbar component (already defined)
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </nav>
+  );
+}
 
+// New About component
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
+// App component
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
